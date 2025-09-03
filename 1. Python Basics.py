ccda402faf35654b174f7e84eeca63f465847829
Aizhan Dussanova <aizhan_dussanova@epam.com>
2025-09-03 19:33:33 +0500

    sort list from min to max(without using sort())

--- 1. Python Basics.py
+++ 1. Python Basics.py
@@ -5,4 +5,21 @@ a = []
 for i in range(100):
     a.append(random.randint(0, 1000))
     #print(a[i])
-#print(len(a))
+#print('\n\n')
+
+
+#sort list from min to max(without using sort())
+for i in range(len(a)):
+    aSorted = True
+    for j in range(len(a)-i-1):
+        if a[j] > a[j+1]:
+            a[j], a[j+1] = a[j+1], a[j]
+            aSorted = False
+    if aSorted:
+        break
+
+"""
+for i in range(len(a)):
+    print(a[i])
+"""
+
